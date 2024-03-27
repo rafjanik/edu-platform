5814f58f572c7353f8863e83712f5de503d22baa
Rafał Janik <r.janik@outlook.com>
2024-03-27 16:27:35 +0100

    Add columns to user entity

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,16 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Role } from 'src/shared/enums/roles.enum';
+
+export class CreateUserDto {
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  @ApiProperty()
+  password: string;
+
+  @ApiProperty()
+  role: Role;
+}
